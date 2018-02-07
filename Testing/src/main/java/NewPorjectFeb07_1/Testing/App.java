5a3d645e32184468b48fb6f863c8592cec6e2aa4
c.kranthi.kondaru <c.kranthi.kondaru@HDC1-L-4210427.dir.svc.accenture.com>
2018-02-07 15:41:32 +0530

    Testing

---
+++ Testing/src/main/java/NewPorjectFeb07_1/Testing/App.java
@@ -0,0 +1,13 @@
+package NewPorjectFeb07_1.Testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
